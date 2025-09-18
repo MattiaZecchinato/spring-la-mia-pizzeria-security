88611bda8692d0b36c1f5203b63f238a088d6501
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-18 11:58:27 +0200

    feat: create role entity

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/model/Role.java
@@ -0,0 +1,49 @@
+package org.lessons.java.spring_la_mia_pizzeria_relazioni.model;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank(message = "Name must be not blank or empty")
+    private int name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
